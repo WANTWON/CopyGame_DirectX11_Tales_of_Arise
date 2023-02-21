c4870ab64b80c290e2eda4afea16f10c0d03cf3d
jhj603 <82034869+jhj603@users.noreply.github.com>
2023-02-21 12:48:14 +0900

    로우 공중 r 수정

--- Client/Client/Private/AI_LAW_AIRSKILLR.cpp
+++ Client/Client/Private/AI_LAW_AIRSKILLR.cpp
@@ -99,8 +99,6 @@ CAIState * CAI_LAW_AIRSKILLR::Tick(_float fTimeDelta)
 				}
 				break;
 			}
-
-			pCollisionMgr->Add_CollisionGroup(CCollision_Manager::COLLISION_PBULLET, m_pOwner);
 		}
 		else
 		{
@@ -113,8 +111,6 @@ CAIState * CAI_LAW_AIRSKILLR::Tick(_float fTimeDelta)
 					{
 						pCollisionMgr->Collect_Collider(CCollider::TYPE_SPHERE, m_pLeftFootCollider);
 						m_pLeftFootCollider = nullptr;
-
-						pCollisionMgr->Out_CollisionGroup(CCollision_Manager::COLLISION_PBULLET, m_pOwner);
 					}
 				}
 				break;
@@ -143,8 +139,6 @@ CAIState * CAI_LAW_AIRSKILLR::Tick(_float fTimeDelta)
 						m_pLeftHandCollider = nullptr;
 					}
 				}
-
-				pCollisionMgr->Out_CollisionGroup(CCollision_Manager::COLLISION_PBULLET, m_pOwner);
 				break;
 			}
 		}
@@ -266,6 +260,9 @@ CAIState * CAI_LAW_AIRSKILLR::LateTick(_float fTimeDelta)
 
 	if (m_bIsAnimationFinished)
 	{
+		if (STATETYPE_MAIN == m_eStateType)
+			m_pOwner->Get_Model()->Reset_Anim(CLaw::ANIM::BTL_ATTACK_SANKAMOUSYUUKYAKU_LOOP);
+
 		if (STATETYPE_END == m_eStateType)
 		{
 			m_pOwner->Off_IsFly();
