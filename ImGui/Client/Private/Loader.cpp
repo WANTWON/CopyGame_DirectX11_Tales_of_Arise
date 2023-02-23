58cea216b44c072bf69f29d46fd008b03e249658
Gioi Giuseppe <giuseppe.gioi.1997@gmail.com>
2023-02-23 21:19:14 +0900

    Rondsenpu Effect created.

--- ImGui/Client/Private/Loader.cpp
+++ ImGui/Client/Private/Loader.cpp
@@ -185,11 +185,11 @@ HRESULT CLoader::Loading_ForClient()
 		return E_FAIL;
 
 	//For Effect
-	/*if (FAILED(Loading_ForEffect()))
+	if (FAILED(Loading_ForEffect()))
 		return E_FAIL;
 	 
 	if (FAILED(Loading_ForEffectTexture()))
-		return E_FAIL;*/
+		return E_FAIL;
 
 	lstrcpy(m_szLoadingText, TEXT("Finished"));
 
