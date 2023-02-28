cd2f4e3d98347de789a4ec399d46f4b599e68e46
WANTWON <106663427+WANTWON@users.noreply.github.com>
2023-02-28 17:58:28 +0900

    .

--- Client/Client/Private/Monster.cpp
+++ Client/Client/Private/Monster.cpp
@@ -605,7 +605,7 @@ _int CMonster::Take_Damage(int fDamage, CBaseObj * DamageCauser, _bool bLockOnCh
 	
 	
 
-	m_tStats.m_fLockonSmashGuage += 0.002f;
+	m_tStats.m_fLockonSmashGuage += 0.02f;
 
 	if (m_tStats.m_fLockonSmashGuage >= 4.f)
 		m_tStats.m_fLockonSmashGuage = 4.f;
