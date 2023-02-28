251c92e5bf142db4877bb338eb47fad82ba47669
jhj603 <82034869+jhj603@users.noreply.github.com>
2023-02-28 16:21:49 +0900

    미니게임 1 수정, 로우 지상 공격 후 점프 처리

--- Client/Client/Public/Level_Restaurant.h
+++ Client/Client/Public/Level_Restaurant.h
@@ -24,7 +24,13 @@ public:
 	HRESULT Ready_Layer_Camera(const _tchar* pLayerTag);
 	_bool Get_MiniGameStart() { return m_bMinigameStart; }
 
-	void Increase_Score(_int iScore) { m_iScore += iScore; }
+	void Increase_Score(_int iScore)
+	{
+		m_iScore += iScore;
+
+		if (0 > m_iScore)
+			m_iScore = 0;
+	}
 
 private:
 	CCollision_Manager* m_pCollision_Manager = nullptr;
